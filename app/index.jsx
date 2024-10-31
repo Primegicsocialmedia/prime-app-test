6b9316277a23fa667603d98a36a7dd3fdc3fb61e
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-10-31 18:20:58 +0200

    sign up and sign in

--- app/index.jsx
+++ app/index.jsx
@@ -42,12 +42,12 @@ export default function App() {
 
           <CustomButton
             title="Continue with Email"
-            handlePress={() => router.push("/home")}
+            handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
         </View>
       </ScrollView>
-      {/* <StatusBar backgroundColor="#161622" style='light'/> */}
+      <StatusBar backgroundColor="#161622" style='light'/>
     </SafeAreaView>
   );
 }
